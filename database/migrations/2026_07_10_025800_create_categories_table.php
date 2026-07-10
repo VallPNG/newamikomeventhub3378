e98600802a7ec4da73ebd015e0480a59b72427d6
VallPNG <valedhikafikri@students.amikom.ac.id>
2026-07-11 00:49:32 +0700

    newamikomeventhub3378

--- database/migrations/2026_07_10_025800_create_categories_table.php
+++ database/migrations/2026_07_10_025800_create_categories_table.php
@@ -6,20 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
-        Schema::create('categories', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
+        // Gunakan ifNotExists agar tidak error jika tabel sudah ada
+        if (!Schema::hasTable('categories')) {
+            Schema::create('categories', function (Blueprint $table) {
+                $table->id();
+                $table->string('name');
+                $table->timestamps();
+            });
+        }
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('categories');
